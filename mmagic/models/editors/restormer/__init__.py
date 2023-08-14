af09400fd2ef9b1372d84136a8ea332cd2d2f828
806556801@qq.com <l@...!jn170511>
2023-08-14 14:23:43 +0800

    t

--- mmagic/models/editors/restormer/__init__.py
+++ mmagic/models/editors/restormer/__init__.py
@@ -1,4 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .restormer_net import Restormer
+from .restormer_net_n2n import Restormer_N2N
 
-__all__ = ['Restormer']
+__all__ = ['Restormer','Restormer_N2N']
+
